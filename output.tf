af298190e17413434694406e0a8bfb55db212f1f
Circular-test <nmartinez@corpit-consulting.com>
2019-07-25 15:35:43 -0300

    new output arn

--- output.tf
+++ output.tf
@@ -2,3 +2,10 @@ output "id" {
   description = "The id of the user pool"
   value       = "${aws_cognito_user_pool.pool-1.id}"
 }
+
+output "arn" {
+  description = "The ARN of the user pool."
+  value       = "${aws_cognito_user_pool.pool-1.arn}"
+}
+
+
