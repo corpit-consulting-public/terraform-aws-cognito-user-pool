23fc36b11f938090aa1cbaa1f19f6f1af64b49c4
lobos1995 <entherius@gmail.com>
2019-10-28 14:56:25 -0300

    cambio de sintaxis a terraform 12

--- main.tf
+++ main.tf
@@ -1,46 +1,71 @@
 resource "aws_cognito_user_pool" "pool-1" {
-  name                          = "${var.name}"
-  username_attributes           = "${var.username_attributes}"
-  auto_verified_attributes      = "${var.auto_verified_attributes}"
-  device_configuration          = "${var.device_configuration}"
-  email_configuration           = "${var.email_configuration}"
-  email_verification_subject    = "${var.email_verification_subject}"
-  email_verification_message    = "${var.email_verification_message}"
-  sms_verification_message      = "${var.sms_verification_message}"
-  lambda_config                 = [{
-    create_auth_challenge          = "${var.create_auth_challenge}"
-    custom_message                 = "${var.custom_message}"
-    define_auth_challenge          = "${var.define_auth_challenge}"
-    post_authentication            = "${var.post_authentication}"
-    post_confirmation              = "${var.post_confirmation}"
-    pre_authentication             = "${var.pre_authentication}"
-    pre_sign_up                    = "${var.pre_sign_up}"
-    pre_token_generation           = "${var.pre_token_generation}"
-    user_migration                 = "${var.user_migration}"
-    verify_auth_challenge_response = "${var.verify_auth_challenge_response}"
-  
-  }]
-  password_policy               = [{
-    require_uppercase                = "${var.require_uppercase}"
-    require_lowercase                = "${var.require_lowercase}"
-    require_numbers                  = "${var.require_numbers}"
-    require_symbols                  = "${var.require_symbols}"
-    minimum_length                   = "${var.minimum_length}"
-    #temporary_password_validity_days = "${var.temporary_password_validity_days}"                           
-  }]
-  schema                        = "${var.schema}"
-  mfa_configuration             = "${var.mfa_configuration}"
-  sms_configuration             = {
-    sns_caller_arn              = "${var.sns_caller_arn}"
-    external_id                 = "${var.external_id}"
+  name                          = var.name
+  username_attributes           = var.username_attributes
+  auto_verified_attributes      = var.auto_verified_attributes
+  device_configuration          {
+    challenge_required_on_new_device = var.challenge_required_on_new_device
+    device_only_remembered_on_user_prompt = var.device_only_remembered_on_user_prompt
   }
-  sms_authentication_message    = "${var.sms_authentication_message}"
-  user_pool_add_ons             = "${var.user_pool_add_ons}"
-  #verification_message_template = "${var.verification_message_template}"
-  tags                          = "${var.tags}"
-  admin_create_user_config = [{
-    allow_admin_create_user_only = "${var.allow_admin_create_user_only}"
-    invite_message_template      = "${var.invite_message_template}"
-    unused_account_validity_days = "${var.unused_account_validity_days}"
- }]
+  email_configuration           {
+    reply_to_email_address      = var.reply_to_email_address
+    source_arn                  = var.source_arn
+  }
+  email_verification_subject    = var.email_verification_subject
+  email_verification_message    = var.email_verification_message
+  sms_verification_message      = var.sms_verification_message
+  lambda_config                 {
+    create_auth_challenge           = var.create_auth_challenge
+    custom_message                  = var.custom_message
+    define_auth_challenge           = var.define_auth_challenge
+    post_authentication             = var.post_authentication
+    post_confirmation               = var.post_confirmation
+    pre_authentication              = var.pre_authentication
+    pre_sign_up                     = var.pre_sign_up
+    pre_token_generation            = var.pre_token_generation
+    user_migration                  = var.user_migration
+    verify_auth_challenge_response  = var.verify_auth_challenge_response
+  }
+  password_policy                {
+    require_uppercase                = var.require_uppercase
+    require_lowercase                = var.require_lowercase
+    require_numbers                  = var.require_numbers
+    require_symbols                  = var.require_symbols
+    minimum_length                   = var.minimum_length
+  }
+  schema                       {
+    attribute_data_type          = var.attribute_data_type
+    name                         = var.name
+    developer_only_attribute     = var.developer_only_attribute
+    mutable                      = var.mutable
+    number_attribute_constraints {
+      max_value                  = var.max_value
+      min_value                  = var.min_value
+    }
+    required                     = var.required
+    string_attribute_constraints {
+      max_length                 = var.max_length
+      min_length                 = var.min_length
+    }
+  }
+  mfa_configuration             = var.mfa_configuration
+  sms_configuration              {
+    sns_caller_arn              = var.sns_caller_arn
+    external_id                 = var.external_id
+  }
+  sms_authentication_message    = var.sms_authentication_message
+  user_pool_add_ons             {
+    advanced_security_mode      = var.advanced_security_mode
+  }
+  #verification_message_template { 
+  #}                         #######verification_message_template configuration############
+  
+  tags                          = var.tags
+  admin_create_user_config  {
+    allow_admin_create_user_only = var.allow_admin_create_user_only
+    #invite_message_template      {
+    #  email_subject              = var.email_subject
+    #  sms_message                = var.sms_message
+    #}
+    unused_account_validity_days = var.unused_account_validity_days
+ }
 }
--- variables.tf
+++ variables.tf
@@ -1,114 +1,114 @@
 ###Variables for Cognito User Pool
 
 variable "admin_create_user_config" {
-  type        = "list"
+  type        = map
   description = "The configuration for AdminCreaterUser requests"
-  default     = []
+  default     = {}
 }
 
 variable "alias_attributes" {
-  type        = "list"
+  type        = list
   description = "Attributes supported as an alias for this user pool. Possible values: phone_number, email, or preferred_username. Cconflicts with username_attributes."
   default     = []
 }
 
 variable "auto_verified_attributes" {
-  type        = "list"
+  type        = list
   description = "The attribute to be auto-verified. Possible values: email, phone_number"
   default     = []
 }
 
 variable "device_configuration" {
-  type        = "list"
+  type        = list
   description = "The configuration for the user pool's device tracking"
   default     = []
 }
 
 variable "email_configuration" {
-  type        = "list"
+  type        = list
   description = "The email configuration"
   default     = []
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "The name of the user pool"
 }
 
 variable "email_verification_subject" {
-  type        = "string"
+  type        = string
   description = "A string representing the email verification subject. Conflicts with verification_message_template configuration block email_subject argument"
   default     = "Your verification code"
 }
 
 variable "email_verification_message" {
-  type        = "string"
+  type        = string
   description = "A string representing the email verification message. Conflicts with verification_message_template configuration block email_message argument"
   default     = "Your verification code is {####}"
 }
 
 variable "lambda_config" {
-  type        = "list"
+  type        = list(string)
   description = "A container for the aws Lamda triggers associated with the user pool"
   default     = []
 }
 
 variable "mfa_configuration" {
-  type        = "string"
+  type        = string
   description = "(Default: OFF) Set to enable multifactor authentication. Must be one of the following values (ON, OFF, OPTIONAL)"
   default     = "OFF"
 }
 
 variable "password_policy" {
-  type        = "list"
+  type        = list
   description = "A container for information about the user pool password policy"
   default     = []
 }
 
 variable "schema" {
-  type        = "list"
+  type        = list
   description = "A container with the schema attributes of a user pool. Maximum of 50 attributes"
   default     = []
 }
 
 variable "sms_configuration" {
-  type        = "list"
+  type        = list
   description = "The SMS configuration"
   default     = []
 }
 
 variable "sms_verification_message" {
-  type        = "string"
+  type        = string
   description = "A string representing the SMS verification message. Conficts with verification_message_template configuration block sms_message argument"
   default     = "Your verification code is {####}"
 }
 
 variable "sms_authentication_message" {
-  type        = "string"
+  type        = string
   description = "A string representing the SMS verification message. Conflicts with verification_message_template configuration block sms_message argument" 
   default     = "Your authentication code is {####}."
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map
   description = "A mapping of tags to assign to User Pool"
   default     = {}
 }
 
 variable "username_attributes" {
-  type        = "list"
+  type        = list
   description = "Specifies whether email addresses of phone numbers can be specified as usernames when a user signs up. Conflicts with alias_attributes "
   default     = []
 }
 
 variable "user_pool_add_ons" {
-  type        = "list"
+  type        = list
   description = "Configuration block for user pool add-ons to enable user pool advanced security mode features"
   default     = []
 }
 
 variable "verification_message_template" {
-  type        = "list"
+  type        = list
   description = "The verification messagetemplates configuration"
   default     = []
 }
@@ -117,38 +117,38 @@ variable "verification_message_template" {
 #######Variables for Admin Create User Config#######
 
 variable "allow_admin_create_user_only" {
-  type        = "string"
+  type        = bool
   description = "Set to true if only the administrator is allowed to create user profiles. Set false if users can sign themselves up via an app."
-  default     = ""
+  default     = false
 }
 
 variable "invite_message_template" {
-  type        = "list"
+  type        = list
   description = "The invite message template structure"
   default     = []
 }
 
 variable "unused_account_validity_days" {
-  type        = "string"
+  type        = string
   description = "The user account expiration limit, in days, after which the account is no longer usable"
   default     = "30"
 }
 
 ##Variable for Invite Message template (inside of Admin Create User Config)
 variable "email_message" {
-  type        = "string"
+  type        = string
   description = "The message template for email messages. Must contain {username} and {####} placeholder, for username and temporary password, respectively"
   default     = "<p>Your username is {username} and temporary password is {####}</p>\n<p>Login here: <a href=\"https://login.mvp.quickpivot.app\">https://login.mvp.quickpivot.app</a></p>\n\n<p>Thanks,<p>\n<p>QuickPivot Team<p>"
 }
 
 variable "email_subject" {
-  type        = "string"
+  type        = string
   description = "The subject line for email messages"
-  default     = "Your temporary password for {####}"
+  default     = "Your temporary password for {######}"
 }
 
 variable "sms_message" {
-  type         = "string"
+  type         = string
   description  = "The messagetemplate for SMS messages. Must contain {username} and {####} placeholder, for username and temporary password, respectively"
   default      = "Your username is {username} and temporary password is {####}. If you do not log in within 7 days you'll need a new invite."
 }
@@ -157,91 +157,93 @@ variable "sms_message" {
 #######Variables for Device Configuration#######
 
 variable "challenge_required_on_new_device" {
-  type        = "string"
+  type        = bool
   description = "Indicate whether a challenge is required on a new device. Only application to a new device"
-  default     = ""
+  default     = true
 }
 
-variable "device_only_remembered_one_user_prompt" {
-  type        = "string"
+variable "device_only_remembered_on_user_prompt" {
+  type        = bool
   description = "If a true, a device is only remembered on user prompt (true or false)"
-  default     = ""
+  default     = true
 }
 
 ##############################################
 #######Variable for Email Configuration#######
 
 variable "reply_to_email_address" {
-  type        = "string"
+  type        = string
   description = "The REPLY-TO email address"
   default     = ""
 }
 
 variable "source_arn" {
-  type        = "string"
+  type        = string
   description = "The ARN of the email source"
   default     = ""
 }
+
+
 
 ###############################################
 #######Variables for Lambda Configuration#######
 
 variable "create_auth_challenge" {
-  type        = "string"
+  type        = string
   description = "The ARN of the lambda creating an authentication challenge"
   default     = ""
 }
 
 variable "custom_message" {
-  type        = "string"
+  type        = string
   description = "A custom Message AWS Lambda trigger"
   default     = ""
 }
 
 variable "define_auth_challenge" {
-  type        = "string"
+  type        = string
   description = "Defines the authentication challenge"
   default     = ""
 }
 
 variable "post_authentication" {
-  type        = "string"
+  type        = string
   description = "A post-authentication AWS Lambda trigger"
   default     = ""
 }
 
 variable "post_confirmation" {
-  type        = "string"
+  type        = string
   description = "A post-confirmation AWS Lambda trigger"
   default     = ""
 }
 
 variable "pre_authentication" {
-  type        = "string"
+  type        = string
   description = "A pre-authentication AWS Lambda trigger"
   default     = ""
 }
 
 variable "pre_sign_up" {
-  type        = "string"
+  type        = string
   description = "A pre-registration AWS Lambda trigger"
   default     = ""
 }
 
 variable "pre_token_generation" {
-  type        = "string"
+  type        = string
   description = "Allow to customize identity token claims before token generation"
   default     = ""
 }
 
 variable "user_migration" {
-  type        = "string"
+  type        = string
   description = "The user migration Lambda config type"
   default     = ""
 }
 
 variable "verify_auth_challenge_response" {
-  type        = "string"
+  type        = string
   description = "Verifies the authentication challenge response"
   default     = ""
 }
@@ -250,31 +252,31 @@ variable "verify_auth_challenge_response" {
 #######Variable for Password Policy#######
 
 variable "minimum_length" {
-  type        = "string"
+  type        = string
   description = "The minimum length of the password policy that you have set"
   default     = "8"
 }
 
 variable "require_lowercase" {
-  type        = "string"
+  type        = string
   description = "Whether you have required users to use at least one lowercase letter in their password"
   default     = "false"
 }
 
 variable "require_numbers" {
-  type        = "string"
+  type        = string
   description = "Whether you have required users to use at least one number in their password"
   default     = "false"
 }
 
 variable "require_symbols" {
-  type        = "string"
+  type        = string
   description = "Whether you have required users to use at least on symbol in their password"
   default     = "false"
 }
 
 variable "require_uppercase" {
-  type        = "string"
+  type        = string
   description = "Whether you have required users to use at least one uppercase letter in their password"
   default     = "false"
 }
@@ -282,47 +284,47 @@ variable "require_uppercase" {
 ############################################
 #######Variable for Schema Attributes#######
 
-###NOTE When defining an attribute_data_type of String or Number, the respective attribute constraints configuration block (e.g string_attribute_constraints or number_attribute_contraints) is required to prevent recreation of the Terraform resource. This requirement is true for both standard (e.g. name, email) and custom schema attributes.
+###NOTE When defining an attribute_data_type of string or Number, the respective attribute constraints configuration block (e.g string_attribute_constraints or number_attribute_contraints) is required to prevent recreation of the Terraform resource. This requirement is true for both standard (e.g. name, email) and custom schema attributes.
 
 variable "attribute_data_type" {
-  type        = "string"
-  description = "The attribute data type. Must be one of Boolean, Number, String, DateTime."
-  default     = ""
+  type        = string
+  description = "The attribute data type. Must be one of Boolean, Number, string, DateTime."
+  default     = "Number"
 }
 
 
 variable "developer_only_attribute" {
-  type        = "string"
+  type        = bool
   description = "Specifies whether the attribute type is developer only"
-  default     = ""
+  default     = false
 }
 
 variable "mutable" {
-  type        = "string"
+  type        = bool
   description = "Specifies whether the attribute can be changed once it has been created"
-  default     = ""
+  default     = false
 }
 
 variable "attribute_name" {
-  type        = "string"
+  type        = string
   description = "The name of the attribute"
   default     = ""
 }
 
 variable "number_attribute_constraints" {
-  type        = "string"
+  type        = string
   description = "Specifies the constraints for an attribute of the number type."
   default     = ""
 }
 
 variable "required" {
-  type        = "string"
+  type        = bool
   description = "Specifies whether a user pool attribute is required. If the attribute is required ad the user does not provide a value, registration or sign-in will fail"
-  default     = ""
+  default     = false
 }
 
 variable "string_attribute_constraints" {
-  type        = "list"
+  type        = list
   description = "Specifies the constraints for an attribute of the string type"
   default     = []
 }
@@ -330,27 +332,27 @@ variable "string_attribute_constraints" {
 #Variable Number Attribute Constraints (inside of schema)
 
 variable "max_value" {
-  type        = "string"
+  type        = string
   description = "The maximum value of an attribute that is of the number data type"
   default     = ""
 }
 
 variable "min_value" {
-  type        = "string"
+  type        = string
   description = "The minimum value of an attribute that is of the number data type"
   default     = ""
 }
 
-#Variable String Attribute Constraints (inside of schema)
+#Variable string Attribute Constraints (inside of schema)
 
 variable "max_length" {
-  type        = "string"
+  type        = string
   description = "The maximum length of an attribute value of the string type"
   default     = ""
 }
 
 variable "min_length" {
-  type        = "string"
+  type        = string
   description = "The minimum length of an attribute value of the string type"
   default     = ""
 }
@@ -359,13 +361,13 @@ variable "min_length" {
 #######Variable for SMS Configuration#######
 
 variable "external_id" {
-  type        = "string"
+  type        = string
   description = "he external ID used in IAM role trust relationships. For more information about using external IDs, see How to Use an External ID When Granting Access to Your AWS Resources to a Third Party"
   default     = ""
 }
 
 variable "sns_caller_arn" {
-  type        = "string"
+  type        = string
   description = "The ARN of the Amazon SNS caller. This is usually the IAM role that you've given Cognito permission to assume"
   default     = ""
 }
@@ -373,53 +375,47 @@ variable "sns_caller_arn" {
 ############################################
 #######Variable for User Pool Add-ons#######
 
-variable "advance_security_mode" {
-  type        = "string"
+variable "advanced_security_mode" {
+  type        = string
   description = "The mode for advanced security, must be one of OFF, AUDIT or ENFORCED"
-  default     = ""
+  default     = "OFF"
 }
 
 #########################################################
 #######Variables for Verification Message Template#######
 
 variable "default_email_option" {
-  type        = "string"
+  type        = string
   description = "The default email option. Must be either CONFIRM_WITH_CODE or CONFIRM_WITH_LINK. Default to CONFIRM_WITH_CODE"
   default     = "CONFIRM_WITH_CODE"
 }
 
 variable "message_template_email_message" {
-  type        = "string"
+  type        = string
   description = "The email message template. Must contain the {####} placeholder. Conflicts with email_verification_message argument"
   default     = ""
 }
 
 variable "email_message_by_link" {
-  type        = "string"
+  type        = string
   description = "The email message template for sending a confirmation link to the user, it must contain the {##Click Here##} placeholder."
   default     = ""
 }
 
 variable "message_template_email_subject" {
-  type        = "string"
+  type        = string
   description = "The subject line for the email message template. Conflicts with email_verification_subject argument"
   default     = ""
 }
 
 variable "email_subject_by_link" {
-  type        = "string"
+  type        = string
   description = "The subject line for the email message template for sending a confirmation link to the user."
   default     = ""
 }
 
 variable "message_template_sms_message" {
-  type        = "string"
+  type        = string
   description = "The SMS message template. Must contain the {####} placeholder. Conflicts with sms_verification_message argument."
-  default     = ""
-}
-
-variable "temporary_password_validity_days" {
-  type        = "string"
-  description = "The user account expiration limit, in days, after which the account is no longer usable"
   default     = ""
 }
