831d682dc0bb76002a2aa535be6cc172311fab1c
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-06-03 18:20:41 -0300

    comment var default schema

--- variables.tf
+++ variables.tf
@@ -71,239 +71,239 @@ variable "schema" {
   default     = []
 }
 
-variable "schema_default" {
-  type        = list(string)
-  description = "Cognito user pool default schema take from schema_default.auto.tfvars"
-  default = [
-    {
-      "name"                     = "name"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "given_name"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = true
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "family_name"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = true
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "middle_name"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "nickname"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "preferred_username"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "profile"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "picture"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "website"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "email"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = true
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "email_verified"
-      "attribute_data_type"      = "Boolean"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-    },
-    {
-      "name"                     = "gender"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "birthdate"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "10"
-          "max_length" = "10"
-        },
-      ]
-    },
-    {
-      "name"                     = "zoneinfo"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "locale"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "phone_number"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "address"
-      "attribute_data_type"      = "String"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "string_attribute_constraints" = [
-        {
-          "min_length" = "0"
-          "max_length" = "2048"
-        },
-      ]
-    },
-    {
-      "name"                     = "updated_at"
-      "attribute_data_type"      = "Number"
-      "developer_only_attribute" = false
-      "mutable"                  = true
-      "required"                 = false
-      "number_attribute_constraints" = [
-        {
-          "min_value" = "0"
-        },
-      ]
-    },
-  ]
-}
+##variable "schema_default" {
+##  type        = list(map(string))
+##  description = "Cognito user pool default schema take from schema_default.auto.tfvars"
+##  default = [
+##    {
+##      "name"                     = "name"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "given_name"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = true
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "family_name"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = true
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "middle_name"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "nickname"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "preferred_username"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "profile"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "picture"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "website"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "email"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = true
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "email_verified"
+##      "attribute_data_type"      = "Boolean"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##    },
+##    {
+##      "name"                     = "gender"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "birthdate"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "10"
+##          "max_length" = "10"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "zoneinfo"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "locale"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "phone_number"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "address"
+##      "attribute_data_type"      = "String"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "string_attribute_constraints" = [
+##        {
+##          "min_length" = "0"
+##          "max_length" = "2048"
+##        },
+##      ]
+##    },
+##    {
+##      "name"                     = "updated_at"
+##      "attribute_data_type"      = "Number"
+##      "developer_only_attribute" = false
+##      "mutable"                  = true
+##      "required"                 = false
+##      "number_attribute_constraints" = [
+##        {
+##          "min_value" = "0"
+##        },
+##      ]
+##    },
+##  ]
+##}
 
 variable "sms_configuration" {
   type        = list(string)
